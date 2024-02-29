22956dff667d2f3da2aacec71d900ea1bb309571
chessboy71 <bentegrimohamed@gmail.com>
2024-02-29 23:01:23 +0100

    right and left :D

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Creates a left binary tree child.
+ * binary_tree_insert_right - Creates a left binary tree child.
  * @parent: A pointer to the parent of the node to create.
  * @value: The value to put in the new node.
  *
@@ -9,7 +9,7 @@
  *         Otherwise - a pointer to the new node.
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newl;
 
@@ -21,12 +21,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!newl)
 		return (NULL);
 
-	if (parent->left)
+	if (parent->right)
 	{
-		newl->left = parent->left;
-		parent->left->parent = newl;
+		newl->right = parent->right;
+		parent->right->parent = newl;
 	}
-	parent->left = newl;
+	parent->right = newl;
 
 	return (newl);
 }
