bef622f4a674a41858b60b5e2f137977ae3f6d0d
chessboy71 <bentegrimohamed@gmail.com>
2024-03-01 01:13:09 +0100

    s

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,7 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	else if (tree->right && tree->left == NULL)
+	else if (tree->right != NULL && tree->left == NULL)
 		return (1);
 	return (binary_tree_height(tree->left) + 1);
 }
